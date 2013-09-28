cc392d8f95092d89bdf1a0d4b635148534d2eda7
unknown <christian-jayanda@.SENTOSAGROUP>
2013-09-28 17:45:26 +0700

    commit03

---
+++ src/Christian Jayanda KTP.java
@@ -0,0 +1,24 @@
+//Tugas 1 membuat informasi KTP
+
+package Latihan01.sesi1.bin;
+
+
+public class Christian Jayanda KTP
+{
+	public static void main (String[] data){
+		System.out.println ("NIK	: 327106188005");
+		System.out.println ("Nama	: Christian Jayanda");
+		System.out.println ("Tempat/Tgl Lahir	: Bogor,16-05-1988");
+		System.out.println ("Jenis Kelamin	: Laki-laki		Gol Darah : O")
+		System.out.println ("Alamat	: Pahlawan");
+		System.out.println ("RT/RW	: 02/40");
+		System.out.println ("Kel/Desa	: Empang");
+		System.out.println ("Kecamatan	: Kota Bogor Selatan");
+		System.out.println ("Agama	: Kristen");
+		System.out.println ("Status Perkawinan	: Belum Menikah");
+		System.out.println ("Pekerjaan 	: Karyawan Swasta");
+		System.out.println ("Kewarganegaraan	: Karywan Swasta");
+		System.out.println ("Berlaku Hingga	: 16-05-2018");
+	
+		}
+}
