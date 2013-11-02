eaae2132f0eb666a5379518ac148603056023548
unknown <christian-jayanda@.SENTOSAGROUP>
2013-11-02 19:56:53 +0700

    commit03

---
+++ bin/ulang01.java
@@ -0,0 +1,14 @@
+// perintah control flow/struktur kontrol
+// perintah pertama - contoh 01
+
+package Latihan01.sesi1.bin;
+
+public class ulang01
+{
+	public static void main (String[] Xx)
+	{
+		int [] daftar={20,30,40};
+		for (int x=0 ; x<3 ; x++) 
+			System.out.println ("isi element ke-" +x +"=" +daftar[x]);
+	}
+}
